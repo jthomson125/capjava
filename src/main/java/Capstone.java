4e459f1204fc14e51e0eddcaf0383db01ce904de
jthomson125 <jthomson125@gmail.com>
2022-01-24 15:51:01 -0600

    added second table

--- src/main/java/Capstone.java
+++ src/main/java/Capstone.java
@@ -39,6 +39,23 @@ public class Capstone {
                                                 .setType("INTEGER")
                                                 .setMode("REQUIRED")));
 
+        TableSchema sales_schema =
+                new TableSchema()
+                        .setFields(
+                                Arrays.asList(
+                                        new TableFieldSchema()
+                                                .setName("cust_tier_code")
+                                                .setType("STRING")
+                                                .setMode("REQUIRED"),
+                                        new TableFieldSchema()
+                                                .setName("sku")
+                                                .setType("INTEGER")
+                                                .setMode("REQUIRED"),
+                                        new TableFieldSchema()
+                                                .setName("total_sales_amount")
+                                                .setType("FLOAT")
+                                                .setMode("REQUIRED")));
+
         PCollection<TableRow> views_rows =
                 p.apply(
                         "Read from BigQuery1",
@@ -50,11 +67,31 @@ public class Capstone {
                                         "GROUP BY customers.CUST_TIER_CODE, product_views.SKU")
                                 .usingStandardSql());
 
+        PCollection<TableRow> sales_rows =
+                p.apply(
+                        "Read from BigQuery2",
+                        BigQueryIO.readTableRows()
+                                .fromQuery(
+                                 "SELECT customers.CUST_TIER_CODE AS cust_tier_code, orders.SKU AS sku, ROUND(SUM(orders.ORDER_AMT), 2) AS total_sales_amount\n" +
+                                         "FROM `york-cdf-start.final_input_data.customers` AS customers\n" +
+                                         "JOIN `york-cdf-start.final_input_data.orders` AS orders ON (customers.CUSTOMER_ID = orders.CUSTOMER_ID)\n" +
+                                         "GROUP BY customers.CUST_TIER_CODE, orders.SKU"
+                                )
+                                .usingStandardSql());
+
         views_rows.apply(
                 "Write to BigQuery1",
                 BigQueryIO.writeTableRows()
                         .to("york-cdf-start:final_james_thomson.cust_tier_code-sku-total_no_of_product_views_java")
                         .withSchema(views_schema)
+                        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+                        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
+
+        sales_rows.apply(
+                "Write to BigQuery2",
+                BigQueryIO.writeTableRows()
+                        .to("york-cdf-start:final_james_thomson.cust_tier_code-sku-total_sales_amount_java")
+                        .withSchema(sales_schema)
                         .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
 
